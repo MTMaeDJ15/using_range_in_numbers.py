88705355605abb20ec87e273138f7c895a133104
unknown <mariateresamaedj19@gmail.com>
2024-10-30 01:11:25 +0800

    file_try_#2.py seems to be the purpose of the code is split the inputted number instead of categorizing while the original file the list is working properly but the number inputted are all the same

--- file_try_#2.py
+++ file_try_#2.py
@@ -1,20 +1,3 @@
-# - ask the user to input a NUMBER ranges from 1 to 50
-         # > Less than or equal to 50 = VALID
-         # > Greater than or equal to 51 = INVALID
-         # > User input is not limited unless it's invalid
-# - ask the user to input again until the user input is invalid
-# - when the user input is invalid
-         # > Display how many inputted numbers are in the following range:
-         # > VALID NUM. = ASK NEXT NUM.
-         # > INVALID NUM. = DISPLAY THE TALLY OF INPUTS IN THE RANGES THEY BELONG
-         # > ex. 
-            # Display should be like this:
-              # 1 to 10: 3, 5, 8, 9, 
-              # 11 to 20: 11, 15, 18, 20
-              # 21 to 30: 24, 26, 28, 30
-              # 31 to 40: 35, 38, 39, 
-              # 41 to 50: 43, 45, 46, 
-
 def collect_data():
     data_entered = []  # This will be the list storage for the information inputted by the user.
 
@@ -48,11 +31,11 @@ def categorize(data_entered):
     thirtyone_to_forty = []
     fortyone_to_fifty = []
 
-    one_to_ten = data_entered
-    eleven_to_twenty = data_entered
-    twentyone_to_thirty = data_entered
-    thirtyone_to_forty = data_entered
-    fortyone_to_fifty = data_entered
+    one_to_ten = data_entered[0:10]
+    eleven_to_twenty = data_entered[10:20]
+    twentyone_to_thirty = data_entered[20:30]
+    thirtyone_to_forty = data_entered[30:40]
+    fortyone_to_fifty = data_entered[40:50]
 
     print("1 to 10:", one_to_ten)
     print("11 to 20:", eleven_to_twenty)
@@ -68,9 +51,3 @@ def main(): #To define the whole program collect, print, and categorize.
 
 if __name__ == "__main__":
     main()
-
-
-    
-
-
-
